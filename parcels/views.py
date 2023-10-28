d2ac0806a9580c8552cbef039567547b25ca551c
Leo Suarez <leo@dtraleigh.com>
2023-10-28 16:10:03 -0400

    Created the lat-lon form and return all parcels where this point is inside.

--- parcels/views.py
+++ parcels/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from parcels.location import get_parcels_from_point
+
 
 def main(request):
-    return render(request, "parcel_main.html", {})
+    lat_string_from_form = request.GET.get("lat_input")
+    lat = float(lat_string_from_form)
+    lon_string_from_form = request.GET.get("lon_input")
+    lon = float(lon_string_from_form)
+
+    main_parcels = get_parcels_from_point(lat, lon)
+
+    return render(request, "parcel_main.html", {"main_parcels": main_parcels})
