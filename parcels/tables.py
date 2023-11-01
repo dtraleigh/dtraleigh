4dc5a47824eae4c6cbf2714c08395474a225711b
Leo Suarez <leo@dtraleigh.com>
2023-11-01 11:13:54 -0400

    Updated table styles.

--- parcels/tables.py
+++ parcels/tables.py
@@ -3,7 +3,10 @@ from parcels.models import Parcel
 
 
 class ParcelHistoryTable(tables.Table):
+    modified_date = tables.DateTimeColumn(format="M d, Y")
+
     class Meta:
         model = Parcel
+        attrs = {"class": "table table-bordered table-hover table-sm table-responsive"}
         template_name = "django_tables2/bootstrap.html"
-        fields = ("modified_date", )
+        fields = ("modified_date",)
