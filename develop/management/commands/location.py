7619c97f8d70c01e9c3b93c12f18478725e0c552
Leo Suarez <leo@dtraleigh.com>
2023-12-31 13:50:45 -0500

    catching a keyerror exception in location.get_lat_lon_by_pin()

--- develop/management/commands/location.py
+++ develop/management/commands/location.py
@@ -152,6 +152,12 @@ def get_lat_lon_by_pin(pin):
             message += str(response.text.encode('utf8'))
             send_email_notice(message, email_admins())
             return None, None
+        except KeyError:
+            message = "KeyError in location.get_lat_lon_by_pin()\n"
+            message += str(response.text.encode('utf8'))
+            send_email_notice(message, email_admins())
+            return None, None
+
     return None, None
 
 
