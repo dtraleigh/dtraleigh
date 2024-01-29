2bf33024a85e13e17c64290a74b429eb4c1d0742
Leo Suarez <leo@dtraleigh.com>
2024-01-29 12:20:47 -0500

    Refined the filter script and adding delete logic to remove parcels that are not in Raleigh.

--- parcels/management/commands/filter_processed_parcels.py
+++ parcels/management/commands/filter_processed_parcels.py
@@ -3,6 +3,7 @@
 import sys
 
 from django.core.management.base import BaseCommand
+from django.core.paginator import Paginator
 
 from parcels.models import ParcelHistorical
 from parcels.parcel_archive.functions import parcel_has_CITY_value, get_list_of_all_possible_CITY_values
@@ -14,26 +15,35 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-        increment = 10000
-        page = 0
-        total_parcels = ParcelHistorical.objects.count()
-        print(total_parcels)
+        paginator = Paginator(ParcelHistorical.objects.all(), 10000)
+        parcel_historical_ids_to_delete = []
 
-        print(f"Page: ", " ")
+        print(f"{paginator.num_pages} pages with {paginator.count} parcels total.")
+        print(f"Page: ", end=" ")
+
         all_CITY_values_found = []
-        while page < total_parcels:
-            all_parcel_subset = ParcelHistorical.objects.all()[page:page+increment]
-            print(f"{page}", sep=" ", end=" ", flush=True)
+        for page_number in paginator.page_range:
+            page = paginator.page(page_number)
+            print(f"{page_number}", sep=" ", end=" ", flush=True)
 
             # Validate that the parcels can be filtered by checking a variety of methods
             # Method 1: Parcel geojson contains a CITY field = RAL or some other similar indicator
-            for loop_count, parcel in enumerate(all_parcel_subset):
+            for parcel in page.object_list:
                 # Check that the parcel even has the field
                 if not parcel_has_CITY_value(parcel):
-                    print(f"Check {parcel}: {parcel.data_geosjon}")
+                    print(f"Check {parcel}: {parcel.data_geojson}")
                     sys.exit(1)
-            page += increment
-            all_CITY_values_found = all_CITY_values_found + get_list_of_all_possible_CITY_values(all_parcel_subset)
+                if parcel.data_geojson["properties"]["CITY"] != "RAL":
+                    parcel_historical_ids_to_delete.append(parcel.id)
+                    print(parcel)
+                    sys.exit(1)
+
+            all_CITY_values_found = all_CITY_values_found + get_list_of_all_possible_CITY_values(page.object_list)
             all_CITY_values_found = list(set(all_CITY_values_found))
 
-        print(all_CITY_values_found)
+        print(f"all_CITY_values_found: {all_CITY_values_found}")
+
+        print('Deleting parcels where parcel.data_geojson["properties"]["CITY"] != "RAL"')
+        ParcelHistorical.objects.filter(id__in=parcel_historical_ids_to_delete).delete()
+
+        print(f"Parcels left: {ParcelHistorical.objects.count()}")
--- parcels/models.py
+++ parcels/models.py
@@ -61,5 +61,8 @@ class ParcelHistorical(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["id"]
+
     def __str__(self):
         return f"ParcelHistorical (id:{self.id})"
